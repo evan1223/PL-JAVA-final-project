b6957185587d842a56e6fc06364594c164aa0f0e
evan1223 <evanho8040@gmail.com>
2025-04-22 22:15:40 +0800

    refactor: Integrate the program

---
+++ src/main/java/com/demo/controller/LoginController.java
@@ -0,0 +1,47 @@
+package com.demo.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import com.demo.util.*;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.demo.service.*;
+
+import java.io.IOException;
+
+@Component
+public class LoginController {
+
+    @FXML
+    private TextField inputUsername;
+
+    @FXML
+    private PasswordField inputPassword;
+
+    @FXML
+    private Label errorLabel;
+
+    @Autowired
+    private AuthService authService;
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        String username = inputUsername.getText();
+        String password = inputPassword.getText();
+
+        if (authService.authenticate(username, password)) {
+            errorLabel.setText("");
+            try {
+                SceneManager.switchScene("/fxml/main.fxml", "/styles/main.css", "Main Page");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            errorLabel.setText("Invalid credentials");
+        }
+    }
+}
+
